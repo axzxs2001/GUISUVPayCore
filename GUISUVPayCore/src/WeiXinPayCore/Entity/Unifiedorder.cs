e7eecbbcf843ce9b8d6e6cb055a4f0e9f00be303
LiuHang <liuxing@xinhis.com>
2017-03-13 22:41:48 +0800

    PayCore: 添加微信统一下单上传和返回实体类

--- GUISUVPayCore/src/WeiXinPayCore/Entity/Unifiedorder.cs
+++ GUISUVPayCore/src/WeiXinPayCore/Entity/Unifiedorder.cs
@@ -5,34 +5,85 @@ using System.Text;
 namespace WeiXinPayCore.Entity
 {
     /// <summary>
-    /// 
+    /// 统一下单实体类
     /// </summary>
-    [Trade("https://api.mch.weixin.qq.com/pay/unifiedorder")]
-    public class Unifiedorder:WeiXinPayParameters
+    [Trade("https://api.mch.weixin.qq.com/pay/unifiedorder", RequireCertificate = false)]
+    public class Unifiedorder : WeiXinPayParameters
     {
         /// <summary>
         /// 商品描述
         /// </summary>
         [TradeField("body", Length = 128, IsRequire = true)]
-        public string Body
-        { get; set; }
+        public string Body { get; set; }
         /// <summary>
         /// 商品详情
         /// </summary>
-        [TradeField("detail", Length = 6000)]
-        public string Detail
-        { get; set; }
+        [TradeField("detail", Length = 6000, IsRequire = false)]
+        public string Detail{ get; set; }
         /// <summary>
-        /// 商品详情
+        /// 附加数据
         /// </summary>
-        [TradeField("attach", Length = 127)]
-        public string Attach
-        { get; set; }
+        [TradeField("attach", Length = 127, IsRequire = false)]
+        public string Attach { get; set; }
         /// <summary>
-        /// 商品详情
+        /// 商户订单号
         /// </summary>
-        [TradeField("out_trade_no", Length = 127)]
-        public string OutTradeNo
-        { get; set; }
+        [TradeField("out_trade_no", Length = 32, IsRequire = true)]
+        public string OutTradeNo { get; set; }
+        /// <summary>
+        /// 标价币种
+        /// </summary>
+        [TradeField("fee_type,", Length = 16, IsRequire = false)]
+        public string FeeType { get; set; }
+        /// <summary>
+        /// 标价金额
+        /// </summary>
+        [TradeField("total_fee", IsRequire = true)]
+        public int ToalFee { get; set; }
+        /// <summary>
+        /// 终端IP
+        /// </summary>
+        [TradeField("spbill_create_ip", Length = 16, IsRequire = true)]
+        public string SpbillCreateIP { get; set; }
+        /// <summary>
+        /// 交易起始时间
+        /// </summary>
+        [TradeField("time_start", Length = 14, IsRequire = false)]
+        public string TimeStart { get; set; }
+        /// <summary>
+        /// 交易结束时间
+        /// </summary>
+        [TradeField("time_expire", Length = 14, IsRequire = false)]
+        public string TimeExpire { get; set; }
+        /// <summary>
+        /// 商品标记
+        /// </summary>
+        [TradeField("goods_tag", Length = 32, IsRequire = false)]
+        public string GoodsTag { get; set; }
+        /// <summary>
+        /// 通知地址
+        /// </summary>
+        [TradeField("notify_url", Length = 256, IsRequire = true)]
+        public string NotifyURL { get; set; }
+        /// <summary>
+        /// 交易类型
+        /// </summary>
+        [TradeField("trade_type", Length = 16, IsRequire = true)]
+        public string TradeType { get; set; }
+        /// <summary>
+        /// 商品ID
+        /// </summary>
+        [TradeField("product_id", Length = 32, IsRequire = false)]
+        public string ProductID { get; set; }
+        /// <summary>
+        /// 指定支付方式
+        /// </summary>
+        [TradeField("limit_pay", Length = 32, IsRequire = false)]
+        public string LimitPay { get; set; }
+        /// <summary>
+        /// 用户标识
+        /// </summary>
+        [TradeField("oppid",Length =128,IsRequire =false)]
+        public string OpenID { get; set; }
     }
 }
--- GUISUVPayCore/src/WeiXinPayCore/Entity/UnifiedorderBack.cs
+++ GUISUVPayCore/src/WeiXinPayCore/Entity/UnifiedorderBack.cs
@@ -4,7 +4,50 @@ using System.Text;
 
 namespace WeiXinPayCore.Entity
 {
+    /// <summary>
+    /// 统一下单返回实体类
+    /// </summary>
     public class UnifiedorderBack: WeiXinPayBackParameters
     {
+        /// <summary>
+        ///返回状态码
+        /// </summary>
+        [TradeField("return_code", Length = 16, IsRequire = true)]
+        public string ReturnCode { get; set; }
+        /// <summary>
+        /// 返回信息
+        /// </summary>
+        [TradeField("return_msg", Length = 128, IsRequire = false)]
+        public string ReturnMsg { get; set; }
+        /// <summary>
+        /// 业务结果
+        /// </summary>
+        [TradeField("result_code", Length = 16, IsRequire = true)]
+        public string ResultCode { get; set; }
+        /// <summary>
+        /// 错误代码
+        /// </summary>
+        [TradeField("err_code",Length =32,IsRequire =false)]
+        public string ErrCode { get; set; }
+        /// <summary>
+        /// 错误代码描述
+        /// </summary>
+        [TradeField("err_code_des", Length = 128, IsRequire = false)]
+        public string ErrCodeDes { get; set; }
+        /// <summary>
+        /// 交易类型
+        /// </summary>
+        [TradeField("trade_type", Length = 16, IsRequire = true)]
+        public string TradeType { get; set; }
+        /// <summary>
+        ///预支付交易会话标识
+        /// </summary>
+        [TradeField("prepay_id", Length = 64, IsRequire = true)]
+        public string PrepayID { get; set; }
+        /// <summary>
+        /// 二维码链接
+        /// </summary>
+        [TradeField("code_url",Length =64,IsRequire =false)]
+        public string CodeURL { get; set; }
     }
 }
